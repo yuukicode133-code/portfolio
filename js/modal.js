e7944cbb54517947082e558b4104f9b3e7d6ece1
user <pandashirokuma91@gmail.com>
2026-02-02 22:29:19 +0900

    モーダルにカスタムカーソルを追加

--- js/modal.js
+++ js/modal.js
@@ -1,62 +1,62 @@
-$(function() {
+$(function () {
   // ===================
   // プロジェクトデータ
   // ===================
   var projectData = {
     a: {
-      title: 'Project A',
-      description: 'コーポレートサイトのデザインから実装まで担当しました。ユーザビリティを重視したUI設計を行いました。',
-      technologies: ['HTML / CSS', 'JavaScript', 'WordPress'],
-      image: 'https://placehold.co/800x500',
-      link: 'https://example.com/project-a'
+      title: "Project A",
+      description: "コーポレートサイトのデザインから実装まで担当しました。ユーザビリティを重視したUI設計を行いました。",
+      technologies: ["HTML / CSS", "JavaScript", "WordPress"],
+      image: "https://placehold.co/800x500",
+      link: "https://example.com/project-a",
     },
     b: {
-      title: 'Project B',
-      description: 'ECサイトのUI/UXデザインを担当。購入導線の最適化により、CVRが30%向上しました。',
-      technologies: ['Figma', 'Adobe XD', 'Photoshop'],
-      image: 'https://placehold.co/800x500',
-      link: 'https://example.com/project-b'
+      title: "Project B",
+      description: "ECサイトのUI/UXデザインを担当。購入導線の最適化により、CVRが30%向上しました。",
+      technologies: ["Figma", "Adobe XD", "Photoshop"],
+      image: "https://placehold.co/800x500",
+      link: "https://example.com/project-b",
     },
     c: {
-      title: 'Project C',
-      description: 'スタートアップ企業のブランディングとWebサイト制作を担当しました。',
-      technologies: ['HTML / CSS', 'JavaScript', 'Illustrator'],
-      image: 'https://placehold.co/800x500',
-      link: 'https://example.com/project-c'
+      title: "Project C",
+      description: "スタートアップ企業のブランディングとWebサイト制作を担当しました。",
+      technologies: ["HTML / CSS", "JavaScript", "Illustrator"],
+      image: "https://placehold.co/800x500",
+      link: "https://example.com/project-c",
     },
     d: {
-      title: 'Project D',
-      description: '新サービスローンチに向けたランディングページを制作しました。',
-      technologies: ['HTML / CSS', 'JavaScript', 'GSAP'],
-      image: 'https://placehold.co/800x500',
-      link: 'https://example.com/project-d'
+      title: "Project D",
+      description: "新サービスローンチに向けたランディングページを制作しました。",
+      technologies: ["HTML / CSS", "JavaScript", "GSAP"],
+      image: "https://placehold.co/800x500",
+      link: "https://example.com/project-d",
     },
     e: {
-      title: 'Project E',
-      description: 'WordPressによるオリジナルテーマ開発を行いました。',
-      technologies: ['WordPress', 'PHP', 'MySQL', 'JavaScript'],
-      image: 'https://placehold.co/800x500',
-      link: 'https://example.com/project-e'
+      title: "Project E",
+      description: "WordPressによるオリジナルテーマ開発を行いました。",
+      technologies: ["WordPress", "PHP", "MySQL", "JavaScript"],
+      image: "https://placehold.co/800x500",
+      link: "https://example.com/project-e",
     },
     f: {
-      title: 'Project F',
-      description: '大手企業のコーポレートサイトリニューアルを担当しました。',
-      technologies: ['HTML / CSS', 'JavaScript', 'WordPress'],
-      image: 'https://placehold.co/800x500',
-      link: 'https://example.com/project-f'
-    }
+      title: "Project F",
+      description: "大手企業のコーポレートサイトリニューアルを担当しました。",
+      technologies: ["HTML / CSS", "JavaScript", "WordPress"],
+      image: "https://placehold.co/800x500",
+      link: "https://example.com/project-f",
+    },
   };
 
   // ===================
   // 変数定義
   // ===================
-  var $modal = $('#works-modal');
+  var $modal = $("#works-modal");
   var modal = $modal[0]; // ネイティブのdialog要素
-  var $closeBtn = $modal.find('.works-modal__close');
-  var $slides = $modal.find('.works-modal__slide');
-  var $dots = $modal.find('.works-modal__dot');
-  var $prevBtn = $modal.find('.works-modal__arrow--prev');
-  var $nextBtn = $modal.find('.works-modal__arrow--next');
+  var $closeBtn = $modal.find(".works-modal__close");
+  var $slides = $modal.find(".works-modal__slide");
+  var $dots = $modal.find(".works-modal__dot");
+  var $prevBtn = $modal.find(".works-modal__arrow--prev");
+  var $nextBtn = $modal.find(".works-modal__arrow--next");
 
   var currentSlide = 0;
   var totalSlides = $slides.length;
@@ -65,13 +65,13 @@ $(function() {
   // ===================
   // モーダルを開く
   // ===================
-  $('.works__item').on('click', function(e) {
+  $(".works__item").on("click", function (e) {
     e.preventDefault();
 
     if (isAnimating) return;
 
     var index = $(this).index();
-    var projectKeys = ['a', 'b', 'c', 'd', 'e', 'f'];
+    var projectKeys = ["a", "b", "c", "d", "e", "f"];
     var projectKey = projectKeys[index];
 
     openModal(projectKey);
@@ -82,10 +82,16 @@ $(function() {
     isAnimating = true;
 
     currentSlide = 0;
-
+    
+    // カスタムカーソルをモーダル内に移動
+    var $cursor = $(".custom-cursor");
+    if ($cursor.length) {
+      $modal.append($cursor);
+    }
+    
     // プロジェクトクラスをリセット＆追加
-    $modal.removeClass('is-project-a is-project-b is-project-c is-project-d is-project-e is-project-f is-closing');
-    $modal.addClass('is-project-' + projectKey);
+    $modal.removeClass("is-project-a is-project-b is-project-c is-project-d is-project-e is-project-f is-closing");
+    $modal.addClass("is-project-" + projectKey);
 
     // コンテンツを更新
     updateModalContent(projectKey);
@@ -97,10 +103,10 @@ $(function() {
     modal.showModal();
 
     // 次のフレームでアニメーション開始
-    requestAnimationFrame(function() {
-      $modal.addClass('is-open');
+    requestAnimationFrame(function () {
+      $modal.addClass("is-open");
 
-      setTimeout(function() {
+      setTimeout(function () {
         isAnimating = false;
       }, 500);
     });
@@ -110,21 +116,21 @@ $(function() {
     var data = projectData[projectKey];
 
     // タイトルと説明
-    $modal.find('.works-modal__title').text(data.title);
-    $modal.find('.works-modal__description').text(data.description);
+    $modal.find(".works-modal__title").text(data.title);
+    $modal.find(".works-modal__description").text(data.description);
 
     // 技術リスト
-    var $techList = $modal.find('.works-modal__tech-list');
+    var $techList = $modal.find(".works-modal__tech-list");
     $techList.empty();
-    data.technologies.forEach(function(tech) {
-      $techList.append('<li>' + tech + '</li>');
+    data.technologies.forEach(function (tech) {
+      $techList.append("<li>" + tech + "</li>");
     });
 
     // 画像
-    $modal.find('.works-modal__image img').attr('src', data.image);
+    $modal.find(".works-modal__image img").attr("src", data.image);
 
     // リンク
-    $modal.find('.works-modal__button').attr('href', data.link);
+    $modal.find(".works-modal__button").attr("href", data.link);
   }
 
   // ===================
@@ -135,21 +141,25 @@ $(function() {
     isAnimating = true;
 
     // 閉じるアニメーション開始
-    $modal.addClass('is-closing');
-    $modal.removeClass('is-open');
+    $modal.addClass("is-closing");
+    $modal.removeClass("is-open");
 
     // アニメーション完了後にdialogを閉じる
-    setTimeout(function() {
+    setTimeout(function () {
+      var $cursor = $(".custom-cursor");
+      if ($cursor.length) {
+        $("body").append($cursor);
+      }
       modal.close();
-      $modal.removeClass('is-closing');
+      $modal.removeClass("is-closing");
       isAnimating = false;
     }, 400);
   }
 
-  $closeBtn.on('click', closeModal);
+  $closeBtn.on("click", closeModal);
 
   // 背景（::backdrop）クリックで閉じる
-  $modal.on('click', function(e) {
+  $modal.on("click", function (e) {
     // クリックがdialog自体（backdrop部分）の場合のみ閉じる
     if (e.target === modal) {
       closeModal();
@@ -157,7 +167,7 @@ $(function() {
   });
 
   // Escapeキーでの閉じる処理をカスタマイズ（アニメーション付きで閉じる）
-  $modal.on('cancel', function(e) {
+  $modal.on("cancel", function (e) {
     e.preventDefault();
     closeModal();
   });
@@ -167,16 +177,16 @@ $(function() {
   // ===================
   function updateSlide() {
     // スライド切り替え
-    $slides.removeClass('is-active');
-    $slides.eq(currentSlide).addClass('is-active');
+    $slides.removeClass("is-active");
+    $slides.eq(currentSlide).addClass("is-active");
 
     // ドット更新
-    $dots.removeClass('is-active');
-    $dots.eq(currentSlide).addClass('is-active');
+    $dots.removeClass("is-active");
+    $dots.eq(currentSlide).addClass("is-active");
 
     // ボタン状態更新
-    $prevBtn.prop('disabled', currentSlide === 0);
-    $nextBtn.prop('disabled', currentSlide === totalSlides - 1);
+    $prevBtn.prop("disabled", currentSlide === 0);
+    $nextBtn.prop("disabled", currentSlide === totalSlides - 1);
   }
 
   function nextSlide() {
@@ -193,26 +203,26 @@ $(function() {
     }
   }
 
-  $nextBtn.on('click', nextSlide);
-  $prevBtn.on('click', prevSlide);
+  $nextBtn.on("click", nextSlide);
+  $prevBtn.on("click", prevSlide);
 
   // ドットクリック
-  $dots.on('click', function() {
-    currentSlide = $(this).data('index');
+  $dots.on("click", function () {
+    currentSlide = $(this).data("index");
     updateSlide();
   });
 
   // ===================
   // キーボード操作
   // ===================
-  $(document).on('keydown', function(e) {
+  $(document).on("keydown", function (e) {
     if (!modal.open) return;
 
-    switch(e.key) {
-      case 'ArrowRight':
+    switch (e.key) {
+      case "ArrowRight":
         nextSlide();
         break;
-      case 'ArrowLeft':
+      case "ArrowLeft":
         prevSlide();
         break;
     }
@@ -224,11 +234,11 @@ $(function() {
   var touchStartX = 0;
   var touchEndX = 0;
 
-  $modal.on('touchstart', function(e) {
+  $modal.on("touchstart", function (e) {
     touchStartX = e.originalEvent.changedTouches[0].screenX;
   });
 
-  $modal.on('touchend', function(e) {
+  $modal.on("touchend", function (e) {
     touchEndX = e.originalEvent.changedTouches[0].screenX;
     handleSwipe();
   });
