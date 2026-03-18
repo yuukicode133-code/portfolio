daf67a521959350cedede659e084f15ed5303f9f
code133 <pandashirokuma91@gmail.com>
2026-03-18 23:37:26 +0900

    一旦最初から。

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  base: '/portfolio/',
   root: 'src',
   build: {
     outDir: '../dist',
