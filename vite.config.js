6a32bb037e5bd278ec37480f372c8e563a03710e
code133 <pandashirokuma91@gmail.com>
2026-07-14 19:08:06 +0900

    ビルド失敗→修正2

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,35 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import handlebars from "vite-plugin-handlebars";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
-  root: "src",
-  base: "/portfolio/",
+  root: 'src',
+  base: '/portfolio/',
   build: {
-    outDir: "../dist",
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        index: resolve(__dirname, "src/index.html"),
-        contact: resolve(__dirname, "src/contact/index.html"),
-        confirm: resolve(__dirname, "src/contact/confirm/index.html"),
-        thanks: resolve(__dirname, "src/contact/thanks/index.html"),
-        about: resolve(__dirname, "src/about/index.html"),
-        works: resolve(__dirname, "src/works/index.html"),
-        single: resolve(__dirname, "src/single/index.html"),
+        index:   resolve(__dirname, 'src/index.html'),
+      contact: resolve(__dirname, 'src/contact/index.html'),
+      confirm: resolve(__dirname, 'src/contact/confirm/index.html'),
+      thanks:  resolve(__dirname, 'src/contact/thanks/index.html'),
+      about:   resolve(__dirname, 'src/about/index.html'),
+      works:   resolve(__dirname, 'src/works/index.html'),
+      single:  resolve(__dirname, 'src/single/index.html'),
       },
     },
   },
   server: {
     open: true,
   },
-  publicDir: "../public",
+  publicDir: '../public',
   plugins: [
     handlebars({
-      partialDirectory: resolve(__dirname, "src/partials"),
+      partialDirectory: resolve(__dirname, 'src/partials'),
       context(pagePath) {
         return {
-          isTop: pagePath === "/index.html",
+          isTop: pagePath === '/index.html',
         };
       },
     }),
